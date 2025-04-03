37cf8f87c058e7b9a6e461349140339fc3478793
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-03 14:49:07 +0200

    Specifying the type of a local variable

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -24,3 +24,10 @@ Console.WriteLine($"The length of something is {something.Length}");
 
 // Output the type of the something variable.
 Console.WriteLine($"something is a {something.GetType()}");
+
+int population = 67_000_000; // 67 million in UK.
+double weight = 1.88; // in kilograms.
+decimal price = 4.99M; // in pounds sterling.
+string fruit = "Apples"; // string values use double-quotes.
+char letter = 'Z'; // char values use single-quotes.
+bool happy = true; // Booleans can only be true or false.
