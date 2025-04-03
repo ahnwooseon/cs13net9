d57cf6fc72a8aa1aa8a3ae68ff37a7f149946b87
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-03 13:53:14 +0200

    Writing code to explore number sizes

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -29,3 +29,7 @@ Console.WriteLine($"{hexadecimalNotation:N0}");
 Console.WriteLine($"{decimalNotation:X}");
 Console.WriteLine($"{binaryNotation:X}");
 Console.WriteLine($"{hexadecimalNotation:X}");
+
+Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
