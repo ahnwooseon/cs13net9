299db0401950e16ab0343d0bad05b0fed788074d
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-04 04:57:38 +0200

    Understanding format strings

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -32,3 +32,21 @@ Console.WriteLine(
     "Education",
     "Optimizely"
 );
+
+// The following statement must be all on one line when using C# 10
+// or earlier. If using C# 11 or later, we can include a line break
+// in the middle of an expression but not in the string text.
+Console.WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");
+
+string applesText = "Apples";
+int applesCount = 1234;
+string bananasText = "Bananas";
+int bananasCount = 56789;
+
+Console.WriteLine();
+
+Console.WriteLine(format: "{0,-10} {1,6}", arg0: "Name", arg1: "Count");
+
+Console.WriteLine(format: "{0,-10} {1,6:N0}", arg0: applesText, arg1: applesCount);
+
+Console.WriteLine(format: "{0,-10} {1,6:N0}", arg0: bananasText, arg1: bananasCount);
