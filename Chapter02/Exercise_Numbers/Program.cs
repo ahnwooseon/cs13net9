a5a773c494da87503f466830ccec5ecc9a2c448e
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-05 11:27:27 +0200

    Explore Spectre

--- Chapter02/Exercise_Numbers/Program.cs
+++ Chapter02/Exercise_Numbers/Program.cs
@@ -1,79 +1,94 @@
-﻿WriteLine(new string('-', 106));
-Write("{0, -8}", "Type");
-Write("{0}", "Byte(s) of memory");
-Write("{0, 33}", "Min");
-WriteLine("{0, 48}", "Max");
-WriteLine(new string('-', 106));
+﻿using Spectre.Console;
 
-Write("{0,-8}", "sbyte");
-Write("{0, -2}", sizeof(sbyte));
-Write("{0, 48}", sbyte.MinValue);
-WriteLine("{0, 48}", sbyte.MaxValue);
+Table table = new();
 
-Write("{0,-8}", "byte");
-Write("{0, -2}", sizeof(byte));
-Write("{0, 48}", byte.MinValue);
-WriteLine("{0, 48}", byte.MaxValue);
+table.AddColumn("Type");
+table.AddColumn("Byte(s) of memory");
+table.AddColumn("Min");
+table.AddColumn("Max");
 
-Write("{0,-8}", "short");
-Write("{0, -2}", sizeof(short));
-Write("{0, 48}", short.MinValue);
-WriteLine("{0, 48}", short.MaxValue);
+table.Columns[2].RightAligned();
+table.Columns[3].RightAligned();
 
-Write("{0,-8}", "ushort");
-Write("{0, -2}", sizeof(ushort));
-Write("{0, 48}", ushort.MinValue);
-WriteLine("{0, 48}", ushort.MaxValue);
+table.AddRow(
+    "sbyte",
+    sizeof(sbyte).ToString(),
+    sbyte.MinValue.ToString(),
+    sbyte.MaxValue.ToString()
+);
 
-Write("{0,-8}", "int");
-Write("{0, -2}", sizeof(int));
-Write("{0, 48}", int.MinValue);
-WriteLine("{0, 48}", int.MaxValue);
+table.AddRow("byte", sizeof(byte).ToString(), byte.MinValue.ToString(), byte.MaxValue.ToString());
 
-Write("{0,-8}", "uint");
-Write("{0, -2}", sizeof(uint));
-Write("{0, 48}", uint.MinValue);
-WriteLine("{0, 48}", uint.MaxValue);
+table.AddRow(
+    "short",
+    sizeof(short).ToString(),
+    short.MinValue.ToString(),
+    short.MaxValue.ToString()
+);
 
-Write("{0,-8}", "long");
-Write("{0, -2}", sizeof(long));
-Write("{0, 48}", long.MinValue);
-WriteLine("{0, 48}", long.MaxValue);
+table.AddRow(
+    "ushort",
+    sizeof(ushort).ToString(),
+    ushort.MinValue.ToString(),
+    ushort.MaxValue.ToString()
+);
 
-Write("{0,-8}", "ulong");
-Write("{0, -2}", sizeof(ulong));
-Write("{0, 48}", ulong.MinValue);
-WriteLine("{0, 48}", ulong.MaxValue);
+table.AddRow("int", sizeof(int).ToString(), int.MinValue.ToString(), int.MaxValue.ToString());
+
+table.AddRow("uint", sizeof(uint).ToString(), uint.MinValue.ToString(), uint.MaxValue.ToString());
+
+table.AddRow("long", sizeof(long).ToString(), long.MinValue.ToString(), long.MaxValue.ToString());
+
+table.AddRow(
+    "ulong",
+    sizeof(ulong).ToString(),
+    ulong.MinValue.ToString(),
+    ulong.MaxValue.ToString()
+);
 
 unsafe
 {
-    Write("{0,-8}", "Int128");
-    Write("{0, -2}", sizeof(Int128));
-    Write("{0, 48}", Int128.MinValue);
-    WriteLine("{0, 48}", Int128.MaxValue);
+    table.AddRow(
+        "Int128",
+        sizeof(Int128).ToString(),
+        Int128.MinValue.ToString(),
+        Int128.MaxValue.ToString()
+    );
 
-    Write("{0,-8}", "UInt128");
-    Write("{0, -2}", sizeof(UInt128));
-    Write("{0, 48}", UInt128.MinValue);
-    WriteLine("{0, 48}", UInt128.MaxValue);
+    table.AddRow(
+        "UInt128",
+        sizeof(UInt128).ToString(),
+        UInt128.MinValue.ToString(),
+        UInt128.MaxValue.ToString()
+    );
 
-    Write("{0,-8}", "Half");
-    Write("{0, -2}", sizeof(Half));
-    Write("{0, 48}", Half.MinValue);
-    WriteLine("{0, 48}", Half.MaxValue);
+    table.AddRow(
+        "Half",
+        sizeof(Half).ToString(),
+        Half.MinValue.ToString(),
+        Half.MaxValue.ToString()
+    );
 }
 
-Write("{0,-8}", "float");
-Write("{0, -2}", sizeof(float));
-Write("{0, 48}", float.MinValue);
-WriteLine("{0, 48}", float.MaxValue);
+table.AddRow(
+    "float",
+    sizeof(float).ToString(),
+    float.MinValue.ToString(),
+    float.MaxValue.ToString()
+);
 
-Write("{0,-8}", "double");
-Write("{0, -2}", sizeof(double));
-Write("{0, 48}", double.MinValue);
-WriteLine("{0, 48}", double.MaxValue);
+table.AddRow(
+    "double",
+    sizeof(double).ToString(),
+    double.MinValue.ToString(),
+    double.MaxValue.ToString()
+);
 
-Write("{0,-8}", "decimal");
-Write("{0, -2}", sizeof(decimal));
-Write("{0, 48}", decimal.MinValue);
-WriteLine("{0, 48}", decimal.MaxValue);
+table.AddRow(
+    "decimal",
+    sizeof(decimal).ToString(),
+    decimal.MinValue.ToString(),
+    decimal.MaxValue.ToString()
+);
+
+AnsiConsole.Write(table);
