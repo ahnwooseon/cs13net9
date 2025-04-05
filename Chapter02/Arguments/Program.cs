fa5917de3256389b7ef0aa8d58197b31f3559498
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-05 10:46:14 +0200

    Setting options with arguments

--- Chapter02/Arguments/Program.cs
+++ Chapter02/Arguments/Program.cs
@@ -1,6 +1,21 @@
-﻿WriteLine($"There are {args.Length} arguments.");
+﻿using System.Xml;
+
+WriteLine($"There are {args.Length} arguments.");
 
 foreach (string arg in args)
 {
     WriteLine(arg);
 }
+
+if (args.Length < 3)
+{
+    WriteLine("You must specify two colors and cursor size, e.g.");
+    WriteLine("dotnet run red yellow 50");
+    return; // Stop running.
+}
+
+ForegroundColor = Enum.Parse<ConsoleColor>(args[0], true);
+
+BackgroundColor = Enum.Parse<ConsoleColor>(args[1], true);
+
+CursorSize = int.Parse(args[2]);
