62b961dea1bcd7735f57531b2968a8dfae541e5a
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-06 01:18:27 +0200

    Exploring unary operators

---
+++ Chapter03/Operators/Program.cs
@@ -0,0 +1,11 @@
+﻿# region Exploring unary operators
+
+int a = 3;
+int b = a++;
+WriteLine($"a is {a}, b is {b}");
+
+int c = 3;
+int d = ++c; // Prefix means increment c before assigning it.
+WriteLine($"c is {c}, d is {d}");
+
+# endregion
