e69663e2e9acb6e1abf29641220d60c1dc621b3b
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-06 01:29:20 +0200

    Exploring logical operators

--- Chapter03/Operators/Program.cs
+++ Chapter03/Operators/Program.cs
@@ -26,3 +26,21 @@ WriteLine($"g is {g:N1}, f is {f}");
 WriteLine($"g / f = {g / f}");
 
 # endregion
+
+# region Exploring logical operators
+
+bool p = true;
+bool q = false;
+WriteLine($"AND | p     | q ");
+WriteLine($"p   | {p & p, -5} | {p & q, -5} ");
+WriteLine($"q   | {q & p, -5} | {q & q, -5} ");
+WriteLine();
+WriteLine($"OR  | p     | q ");
+WriteLine($"p   | {p | p, -5} | {p | q, -5} ");
+WriteLine($"q   | {q | p, -5} | {q | q, -5} ");
+WriteLine();
+WriteLine($"XOR | p     | q ");
+WriteLine($"p   | {p ^ p, -5} | {p ^ q, -5} ");
+WriteLine($"q   | {q ^ p, -5} | {q ^ q, -5} ");
+
+# endregion
