873ea9804aa26c737c4c39d1211db2acd18ceffa
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-06 01:23:22 +0200

    Exploring binary arithmetic operators

--- Chapter03/Operators/Program.cs
+++ Chapter03/Operators/Program.cs
@@ -9,3 +9,20 @@ int d = ++c; // Prefix means increment c before assigning it.
 WriteLine($"c is {c}, d is {d}");
 
 # endregion
+
+# region Exploring binary arithmetic operators
+
+int e = 11;
+int f = 3;
+WriteLine($"e is {e}, f is {f}");
+WriteLine($"e + f = {e + f}");
+WriteLine($"e - f = {e - f}");
+WriteLine($"e * f = {e * f}");
+WriteLine($"e / f = {e / f}");
+WriteLine($"e % f = {e % f}");
+
+double g = 11.0;
+WriteLine($"g is {g:N1}, f is {f}");
+WriteLine($"g / f = {g / f}");
+
+# endregion
