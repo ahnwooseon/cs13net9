3a4bd12bf55cc6cfc5e2c5a7c8e7722e6a46a627
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-06 09:13:41 +0200

    Pattern matching with the switch statement

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -59,3 +59,55 @@ A_label:
 WriteLine($"After A_label");
 
 # endregion
+
+# region Pattern matching with the switch statement
+
+var animals = new Animal?[]
+{
+    new Cat
+    {
+        Name = "Karen",
+        Born = new(year: 2022, month: 8, day: 23),
+        Legs = 4,
+        IsDomestic = true,
+    },
+    null,
+    new Cat { Name = "Mufasa", Born = new(year: 1994, month: 6, day: 12) },
+    new Spider
+    {
+        Name = "Sid Vicious",
+        Born = DateTime.Today,
+        IsVenomous = true,
+    },
+    new Spider { Name = "Captain Furry", Born = DateTime.Today },
+};
+
+foreach (Animal? animal in animals)
+{
+    string message;
+
+    switch (animal)
+    {
+        case Cat fourLeggedCat when fourLeggedCat.Legs == 4:
+            message = $"The cat named {fourLeggedCat.Name} has four legs.";
+            break;
+        case Cat wildCat when wildCat.IsDomestic == false:
+            message = $"The non-domestic cat is named {wildCat.Name}.";
+            break;
+        case Cat cat:
+            message = $"The cat is named {cat.Name}.";
+            break;
+        default: // default is always evaluated last.
+            message = $"{animal.Name} is a {animal.GetType().Name}.";
+            break;
+        case Spider spider when spider.IsVenomous:
+            message = $"The {spider.Name} spider is venomous. Run!";
+            break;
+        case null:
+            message = "The animal is null.";
+            break;
+    }
+    WriteLine($"switch statement: {message}");
+}
+
+# endregion
