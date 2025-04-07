c09c71b379fac7f4cbc3711e060f73623965124e
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-07 09:58:42 +0200

    Fix

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -1,17 +1,28 @@
-﻿int x = 0;
+﻿# region Looping with the while statement
+
+int x = 0;
 while (x < 10)
 {
     WriteLine(x);
     x++;
 }
 
+# endregion
+
+# region Looping with the do statement
+/*
 string? actualPassword = "Pa$$w0rd";
 string password;
+int attempts = 0;
+
 
 do
 {
     Write("Enter your password: ");
     password = ReadLine()!;
-} while (password != actualPassword);
+    attempts++;
+} while (attempts < 3 && password != actualPassword);
 
-WriteLine("Correct!");
+WriteLine(attempts >= 3 && password != actualPassword ? "Incorrect!" : "Correct!");
+*/
+# endregion
