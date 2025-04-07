a7225478833f168b460c173feaaa322a2a9a8853
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-07 09:34:31 +0200

    Simplifying switch statements with switch expressions

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -108,6 +108,20 @@ foreach (Animal? animal in animals)
             break;
     }
     WriteLine($"switch statement: {message}");
+
+    // Simplifying switch statements with switch expressions
+    message = animal switch
+    {
+        Cat fourLeggedCat when fourLeggedCat.Legs == 4 =>
+            $"The cat named {fourLeggedCat.Name} has four legs.",
+        Cat wildCat when wildCat.IsDomestic == false =>
+            $"The non-domestic cat is named {wildCat.Name}.",
+        Cat cat => $"The cat is named {cat.Name}.",
+        Spider spider when spider.IsVenomous => $"The {spider.Name} spider is venomous. Run!",
+        null => "The animal is null.",
+        _ => $"{animal.Name} is a {animal.GetType().Name}.",
+    };
+    WriteLine($"switch expression: {message}");
 }
 
 # endregion
