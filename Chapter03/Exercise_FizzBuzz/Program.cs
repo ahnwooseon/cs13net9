7e2b0346e06ceeb66f76c89b88ea060681f6522a
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-15 07:28:48 +0200

    Practice loops and operators

---
+++ Chapter03/Exercise_FizzBuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿for (int i = 1; i <= 100; i++)
+{
+    string value = i switch
+    {
+        _ when i % 3 == 0 && i % 5 == 0 => "FizzBuzz",
+        _ when i % 3 == 0 => "Fizz",
+        _ when i % 5 == 0 => "Buzz",
+        _ => i.ToString(),
+    };
+
+    Write($"{value}, ");
+
+    if (i % 10 == 0)
+    {
+        WriteLine();
+    }
+}
