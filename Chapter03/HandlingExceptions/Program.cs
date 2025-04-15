1596467825ca37756c7b3a790860b346a29879bf
Ahn Wooseon <ahnwooseon@gmail.com>
2025-04-15 04:45:49 +0200

    Throwing overflow exceptions with the checked statement

--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -24,3 +24,26 @@ catch (Exception ex)
 WriteLine("After parsing");
 
 #endregion
+
+#region Throwing overflow exceptions with the checked statement
+
+try
+{
+    checked
+    {
+        int x = int.MaxValue - 1;
+        WriteLine($"Initial value: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+    }
+}
+catch (OverflowException)
+{
+    WriteLine("The code overflowed but I caught the exception.");
+}
+
+#endregion
