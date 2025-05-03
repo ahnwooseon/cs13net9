6f987b453862ade5cda9ce4556a906572af20324
Ahn Wooseon <ahnwooseon@gmail.com>
2025-05-04 01:00:31 +0200

    Practice exception handling

---
+++ Chapter03/Exercise_Exceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿try
+{
+    Write("Enter a number between 0 and 255: ");
+    string firstEntry = ReadLine()!;
+    byte firstNumber = byte.Parse(firstEntry);
+
+    Write("Enter another number between 0 and 255: ");
+    string secondEntry = ReadLine()!;
+    byte secondNumber = byte.Parse(secondEntry);
+
+    WriteLine($"{firstNumber} divided by {secondNumber} is {firstNumber / secondNumber}");
+}
+catch (OverflowException)
+{
+    WriteLine("The number you entered is too large.");
+}
+catch (FormatException ex)
+{
+    WriteLine(ex.Message);
+    WriteLine("The number you entered is not in the correct format.");
+}
+catch (DivideByZeroException)
+{
+    WriteLine("You cannot divide by zero.");
+}
+catch (Exception ex)
+{
+    WriteLine($"An unexpected error occurred: {ex.Message}");
+}
