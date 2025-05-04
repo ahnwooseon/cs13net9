2357fda2e569600ef217831e061a1300ce37ac2e
Ahn Wooseon <ahnwooseon@gmail.com>
2025-05-04 08:50:28 +0200

    Times table example

---
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -0,0 +1,16 @@
+using System.Linq.Expressions;
+
+partial class Program
+{
+    static void TimesTable(byte number, byte size = 12)
+    {
+        WriteLine($"This is the {number} times table with {size} rows:");
+        WriteLine();
+        
+        for (int row = 1; row <= size; row++)
+        {
+            WriteLine($"{row} x {number} = {row * number}");
+        }
+        WriteLine();
+    }
+}
