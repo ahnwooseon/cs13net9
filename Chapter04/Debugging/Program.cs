a64eb0bd490af55f4911ecdbe19ac75be1ab9034
Ahn Wooseon <ahnwooseon@gmail.com>
2025-05-04 10:04:19 +0200

    Creating code with a deliberate bug

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,16 @@
+﻿#region Creating code with a deliberate bug
+
+double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press Enter to end the app.");
+ReadLine();
+
+double Add(double a, double b)
+{
+    return a * b; // Deliberate bug!
+}
+
+#endregion Creating code with a deliberate bug
